ea1079002f27adbefed3a9ce188bbe87d8c4ea15
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-10-03 16:00:17 -0300

    alterando nome das pastas 2

--- aula86/main.js
+++ aula86/main.js
@@ -1,8 +1,29 @@
-function esperaAi(msg,tmp){
-    setTimeout(()=>{
-        console.log(msg);
-    },tmp);
+function rand(min,max){
+    min *= 1000;
+    max *= 1000;
+
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
+function esperaAi(msg,tmp){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(typeof msg !== 'string'){
+                reject('erroooo')
+                return;
+            }
 
-esperaAi('oi depois de 10s',1000 * 10)
+            resolve(msg.toUpperCase() + '- Passei na promise')
+        },tmp);
+    })
+}
+
+const promises = [
+    esperaAi('Promise 1', rand(1,5)),
+    esperaAi('Promise 2', rand(1,5)),
+    esperaAi(3, rand(1,5)),
+]
+// Promise.all Promise.race 
+Promise.race(promises)
+    .then((valor)=>{ console.log(valor) })
+    .catch((e)=>{ console.log(e) })
